dbec7755966ed38e2eca7dd0feff14d2aa0f3a23
priyakrisv <43440633+priyakrisv@users.noreply.github.com>
2018-10-01 17:20:04 +0530

    Create set110.py

---
+++ set110.py
@@ -0,0 +1,12 @@
+def nearlyEqual(string1,string2):
+string1=('first word')
+string2=('second word')
+if(string1==string2):
+print(True)
+else:
+print(False)
+return nearlyEqual(string1,string2)
+print(nearlyEqual(string1,string2))
+string1=()
+string2=()
+print, nearlyEqual(string1,string2)
